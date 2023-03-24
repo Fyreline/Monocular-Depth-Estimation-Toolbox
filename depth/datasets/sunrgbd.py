57a18fe0751be9f2ae15f2251c21a7430ccdb8f1
Fyreline <mackenzie.reed@rocketmail.com>
2023-03-24 10:48:00 +0000

    update

--- depth/datasets/sunrgbd.py
+++ depth/datasets/sunrgbd.py
@@ -43,7 +43,6 @@ class SUNRGBDDataset(Dataset):
     gt_depth:    SUNRGBD/kv2/kinect2data/000002_2014-05-26_14-23-37_260595134347_rgbf000103-resize/image/0000103.png 
     Args:
         pipeline (list[dict]): Processing pipeline
-        img_dir (str): Path to image directory
         ann_dir (str, optional): Path to annotation directory. Default: None
         split (str, optional): Split txt file. If split is specified, only
             file with suffix in the splits will be loaded. Otherwise, all
@@ -51,62 +50,47 @@ class SUNRGBDDataset(Dataset):
         data_root (str, optional): Data root for img_dir/ann_dir. Default:
             None.
         test_mode (bool): If test_mode=True, gt wouldn't be loaded.
-        depth_scale=256: Default KITTI pre-process. divide 256 to get gt measured in meters (m)
-        garg_crop=True: Following Adabins, use grag crop to eval results.
-        eigen_crop=False: Another cropping setting.
-        min_depth=1e-3: Default min depth value.
-        max_depth=80: Default max depth value.
     """
 
 
     def __init__(self,
                  pipeline,
-                 img_dir,
-                 ann_dir=None,
                  split=None,
                  data_root=None,
                  test_mode=False,
-                 depth_scale=256,
-                 garg_crop=True,
-                 eigen_crop=False,
+                 depth_scale=1000,
                  min_depth=1e-3,
-                 max_depth=80):
-
+                 max_depth=10):
         self.pipeline = Compose(pipeline)
-        self.img_dir = img_dir
-        self.ann_dir = ann_dir
         self.split = split
         self.data_root = data_root
         self.test_mode = test_mode
         self.depth_scale = depth_scale
-        self.garg_crop = garg_crop
-        self.eigen_crop = eigen_crop
-        self.min_depth = min_depth # just for evaluate. (crop gt to certain range)
-        self.max_depth = max_depth # just for evaluate.
+        self.min_depth = min_depth
+        self.max_depth = max_depth
 
         # join paths if data_root is specified
         if self.data_root is not None:
-            if not (self.img_dir is None or osp.isabs(self.img_dir)):
-                self.img_dir = osp.join(self.data_root, self.img_dir)
-            if not (self.ann_dir is None or osp.isabs(self.ann_dir)):
-                self.ann_dir = osp.join(self.data_root, self.ann_dir)
             if not (self.split is None or osp.isabs(self.split)):
                 self.split = osp.join(self.data_root, self.split)
 
         # load annotations
-        self.img_infos = self.load_annotations(self.img_dir, self.ann_dir, self.split)
+        self.img_infos = self.load_annotations(self.data_root, self.split)
         
 
     def __len__(self):
         """Total number of samples of data."""
         return len(self.img_infos)
 
-    def load_annotations(self, img_dir, ann_dir, split):
+    def load_annotations(self, data_root, split):
         """Load annotation from directory.
         Args:
-            img_dir (str): Path to image directory
-            ann_dir (str|None): Path to annotation directory.
-            split (str|None): Split txt file. Split should be specified, only file in the splits will be loaded.
+            img_dir (str): Path to data directory
+            img_suffix (str): Suffix of images.
+            depth_map_suffix (str|None): Suffix of depth maps.
+            split (str|None): Split txt file. If split is specified, only file
+                with suffix in the splits will be loaded. Otherwise, all images
+                in img_dir/ann_dir will be loaded. Default: None
         Returns:
             list[dict]: All image info of dataset.
         """
@@ -117,23 +101,21 @@ class SUNRGBDDataset(Dataset):
             with open(split) as f:
                 for line in f:
                     img_info = dict()
-                    if ann_dir is not None: # benchmark test or unsupervised future
-                        depth_map = line.strip().split(" ")[1]
-                        if depth_map == 'None':
-                            self.invalid_depth_num += 1
-                            continue
-                        img_info['ann'] = dict(depth_map=depth_map)
+                    depth_map = line.strip().split(" ")[1]
+                    if depth_map == 'None':
+                        self.invalid_depth_num += 1
+                        continue
+                    img_info['ann'] = dict(depth_map=osp.join(data_root, remove_leading_slash(depth_map)))
                     img_name = line.strip().split(" ")[0]
-                    img_info['filename'] = img_name
+                    img_info['filename'] = osp.join(data_root, remove_leading_slash(img_name))
                     img_infos.append(img_info)
         else:
-            print("Split should be specified, NotImplementedError")
+            print("Split is None, ERROR")
             raise NotImplementedError 
 
         # github issue:: make sure the same order
         img_infos = sorted(img_infos, key=lambda x: x['filename'])
         print_log(f'Loaded {len(img_infos)} images. Totally {self.invalid_depth_num} invalid pairs are filtered', logger=get_root_logger())
-
         return img_infos
 
     def get_ann_info(self, idx):
@@ -143,14 +125,11 @@ class SUNRGBDDataset(Dataset):
         Returns:
             dict: Annotation info of specified index.
         """
-
         return self.img_infos[idx]['ann']
 
     def pre_pipeline(self, results):
         """Prepare results dict for pipeline."""
         results['depth_fields'] = []
-        results['img_prefix'] = self.img_dir
-        results['depth_prefix'] = self.ann_dir
         results['depth_scale'] = self.depth_scale
 
     def __getitem__(self, idx):
@@ -202,37 +181,22 @@ class SUNRGBDDataset(Dataset):
 
     def get_gt_depth_maps(self):
         """Get ground truth depth maps for evaluation."""
+
         for img_info in self.img_infos:
             depth_map = img_info['ann']['depth_map']
             depth_map_gt = np.asarray(Image.open(depth_map), dtype=np.float32) / self.depth_scale
             yield depth_map_gt
 
-    def eval_kb_crop(self, depth_gt):
-        """Following Adabins, Do kb crop for testing"""
-        height = depth_gt.shape[0]
-        width = depth_gt.shape[1]
-        top_margin = int(height - 352)
-        left_margin = int((width - 1216) / 2)
-        depth_cropped = depth_gt[top_margin: top_margin + 352, left_margin: left_margin + 1216]
-        depth_cropped = np.expand_dims(depth_cropped, axis=0)
-        return depth_cropped
-
     def eval_mask(self, depth_gt):
-        """Following Adabins, Do grag_crop or eigen_crop for testing"""
         depth_gt = np.squeeze(depth_gt)
         valid_mask = np.logical_and(depth_gt > self.min_depth, depth_gt < self.max_depth)
-        if self.garg_crop or self.eigen_crop:
-            gt_height, gt_width = depth_gt.shape
-            eval_mask = np.zeros(valid_mask.shape)
-
-            if self.garg_crop:
-                eval_mask[int(0.40810811 * gt_height):int(0.99189189 * gt_height),
-                          int(0.03594771 * gt_width):int(0.96405229 * gt_width)] = 1
-
-            elif self.eigen_crop:
-                eval_mask[int(0.3324324 * gt_height):int(0.91351351 * gt_height),
-                          int(0.0359477 * gt_width):int(0.96405229 * gt_width)] = 1
+        
+        ##########
+        eval_mask = np.zeros(valid_mask.shape)
+        eval_mask[45:471, 41:601] = 1
         valid_mask = np.logical_and(valid_mask, eval_mask)
+        ###########
+
         valid_mask = np.expand_dims(valid_mask, axis=0)
         return valid_mask
 
@@ -258,18 +222,20 @@ class SUNRGBDDataset(Dataset):
         for i, (pred, index) in enumerate(zip(preds, indices)):
             depth_map = self.img_infos[index]['ann']['depth_map']
 
-            depth_map_gt = np.asarray(Image.open(depth_map), dtype=np.float32) / self.depth_scale
-            depth_map_gt = self.eval_kb_crop(depth_map_gt)
+            # self.depth_scale
+            depthVisData = np.asarray(Image.open(depth_map, 'r'), np.uint16)
+            depthInpaint = np.bitwise_or(np.right_shift(depthVisData, 3), np.left_shift(depthVisData, 16 - 3))
+            depthInpaint = depthInpaint.astype(np.single) / 1000
+            depthInpaint[depthInpaint > 8] = 8
+            pred[pred > 8] = 8
+            depthInpaint = depthInpaint.astype(np.float32)
+            depth_map_gt = depthInpaint
+
+            # depth_map_gt = np.asarray(Image.open(depth_map), dtype=np.float32) / self.depth_scale
+            depth_map_gt = np.expand_dims(depth_map_gt, axis=0)
+
             valid_mask = self.eval_mask(depth_map_gt)
             
-            eval = metrics(depth_map_gt[valid_mask], 
-                           pred[valid_mask], 
-                           min_depth=self.min_depth,
-                           max_depth=self.max_depth)
-            
-            
-            eval = metrics(depth_map_gt[valid_mask], pred[valid_mask])
-
             eval = metrics(depth_map_gt[valid_mask], pred[valid_mask])
             pre_eval_results.append(eval)
 
@@ -290,7 +256,6 @@ class SUNRGBDDataset(Dataset):
             dict[str, float]: Default metrics.
         """
         metric = ["a1", "a2", "a3", "abs_rel", "rmse", "log_10", "rmse_log", "silog", "sq_rel"]
-        
         eval_results = {}
         # test a list of files
         if mmcv.is_list_of(results, np.ndarray) or mmcv.is_list_of(
