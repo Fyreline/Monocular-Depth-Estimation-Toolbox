8ca94a1c2bc329505c4d6f0559d4676612c7ce71
Fyreline <mackenzie.reed@rocketmail.com>
2023-03-24 12:40:41 +0000

    s

--- depth/datasets/sunrgbd.py
+++ depth/datasets/sunrgbd.py
@@ -158,6 +158,11 @@ class SUNRGBDDataset(Dataset):
         img_info = self.img_infos[idx]
         ann_info = self.get_ann_info(idx)
         results = dict(img_info=img_info, ann_info=ann_info)
+        print(img_info)
+        print("")
+        print(ann_info)
+        print("")
+        print(results)
         self.pre_pipeline(results)
         return self.pipeline(results)
 
@@ -172,9 +177,6 @@ class SUNRGBDDataset(Dataset):
 
         img_info = self.img_infos[idx]
         results = dict(img_info=img_info)
-        print(img_info)
-        print("")
-        print(results)
         self.pre_pipeline(results)
         return self.pipeline(results)
 
