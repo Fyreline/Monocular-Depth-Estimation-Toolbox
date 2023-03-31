85ab4d795ab033909bb6f3fc87a21390795b7c0a
Fyreline <mackenzie.reed@rocketmail.com>
2023-03-31 19:07:47 +0100

    fixed data_root

--- configs/_base_/datasets/kitti.py
+++ configs/_base_/datasets/kitti.py
@@ -1,7 +1,7 @@
 # dataset settings
 dataset_type = 'KITTIDataset'
-#data_root = 'data/kitti'
-data_root = 'E:\DepthFormerData\kitti'
+data_root = 'data/kitti'
+#data_root = 'E:\DepthFormerData\kitti'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 crop_size= (352, 704)
